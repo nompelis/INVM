ce13d6311973e7b226ca19f81bf1488e3218120d
IN <nompelis@nobelware.com>
2021-11-12 12:04:11 -0600

    Modified the VM's opcode internal function to take the number of seconds to use for the SLEEP instruction from a register, which is the subsequent byte in the program, interpreted as the register to use.
    In notional "machine code", this instruction will look like this:
    
       SLEEP reg3
    
    which would imply to "sleep a number of seconds based on the integer that
    is stored at register 3". Obviously a well-written program built by a high
    level language compiler will store the integer value in that register, and
    so it will look something like this:
    
       INT_STORE reg3 60
       SLEEP reg3
    
    If found anywhere inside a program, these two instructions will set the
    number "60" inside register "3", and then the sleep()ing instruction will
    be called to use the contents of this register.
    
    (The use of a register with the SLEEP instruction instead of a literal
    number in a program makes more sense because a program will expect that, in
    general, the sleep function will take use a value that is a result of some
    calculation, and therefore the result of prior operations that must be
    stored in memory, and therefore it will be fetched and stored in a register
    for immediate use. This is based on my understanding of how things should
    work.)
    IN 2021/11/12

--- invm_opcodes.c
+++ invm_opcodes.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 #include "invm.h"
 #include "invm_opcodes.h"
@@ -24,21 +25,130 @@ void vm_exit( invm_t *p )
    ++( p->idx );
 }
 
+
 void vm_sleep( invm_t *p )
 {
 #ifdef _DEBUG_
    fprintf( stdout, " [VM]  SLEEP \n");
 #endif
-   // This is where we need to get the value from the register and use it
-   // as the number of seconds to sleep. But for now we will use a default
-   // value, until I properly author this function.
-   int num_sec = 1;
-   sleep( num_sec );
-#ifdef _DEBUG_
-   fprintf( stdout, " [HACK]  Slept for only one second (hard-wired)\n");
+   // advance 1 byte to start with the payload; first the 1-byte register number
+   p->idx += 1;
+
+   // get the register number
+   unsigned char reg = p->prog[ p->idx ];
+#ifdef _DEBUG2_
+   fprintf( stdout, " [DEBUG]  Got register number: \%d \n", reg );
 #endif
 
-   ++( p->idx );
+   // check for the register being valid (we only have so many registers)
+   if( reg >= p->nreg ) {
+#ifdef _DEBUG2_
+      fprintf( stdout, " [DEBUG]  Bad register number: \%d \n", reg );
+#endif
+      // set a flag on 'register error" (should be done by the compiler, but...)
+      p->flags.ir = 1;
+
+      // trigger an error
+      ////// we invoke an error-handler
+
+      return;
+   }
+
+   // check for the register storing an integer (just an FYI to the user)
+   if( p->registers[ reg ].type != INTEGER ) {
+#ifdef _DEBUG2_
+      fprintf( stdout, " [DEBUG]  FYI, register is not storing an integer! \n");
+#endif
+   }
+
+   // we copy the value from the register to a local variable
+   int num_sec = p->registers[ reg ].content.i;
+
+   // execute the instruction
+   sleep( num_sec );
+#ifdef _DEBUG2_
+   fprintf( stdout, " [DEBUG]  Slept for %d sec \n", num_sec );
+#endif
+
+   // advance 1 byte to go to the next instruction
+   p->idx += 1;
+}
+
+
+void vm_int_store( invm_t *p )
+{
+#ifdef _DEBUG_
+   fprintf( stdout, " [VM]  INT_STORE \n");
+#endif
+   // advance 1 byte to start with the payload; first the 1-byte register number
+   p->idx += 1;
+
+   // get the register number
+   unsigned char reg = p->prog[ p->idx ];
+#ifdef _DEBUG2_
+   fprintf( stdout, " [DEBUG]  Got register number: \%d \n", reg );
+#endif
+   p->idx += 1;
+
+   // check for the register being valid
+   if( reg >= p->nreg ) {
+#ifdef _DEBUG2_
+      fprintf( stdout, " [DEBUG]  Bad register number: \%d \n", reg );
+#endif
+      // set a flag on 'register error" (should be done by the compiler, but...)
+      p->flags.ir = 1;
+
+      // trigger an error
+      ////// we invoke an error-handler
+
+      return;
+   }
+
+   // if the register is storing a string, drop the referenced memory chunk
+   if( p->registers[ reg ].type == STRING ) {
+      if( p->registers[ reg ].content.s != NULL ) {
+         free( p->registers[ reg ].content.s );
+         p->registers[ reg ].content.s = NULL;
+      }
+   }
+
+   // get the payload (4 bytes)
+   int num=0;
+   unsigned char uc;
+#ifdef _DEBUG2_
+      fprintf( stdout, " [DEBUG]  Int-by-segment: ");
+#endif
+   uc = p->prog[ p->idx ];
+   num = num | ( ((int) uc) << 24 );
+   p->idx += 1;
+#ifdef _DEBUG2_
+      fprintf( stdout, " %x ", num );
+#endif
+   uc = p->prog[ p->idx ];
+   num = num | ( ((int) uc) << 16 );
+   p->idx += 1;
+#ifdef _DEBUG2_
+      fprintf( stdout, " %x ", num );
+#endif
+   uc = p->prog[ p->idx ];
+   num = num | ( ((int) uc) << 8 );
+   p->idx += 1;
+#ifdef _DEBUG2_
+      fprintf( stdout, " %x ", num );
+#endif
+   uc = p->prog[ p->idx ];
+   num = num | (int) uc;
+   p->idx +=1;
+#ifdef _DEBUG2_
+      fprintf( stdout, " %x \n", num );
+#endif
+
+   // store the integer to the register
+   p->registers[ reg ].content.i = num;
+
+#ifdef _DEBUG2_
+   fprintf( stdout, " [DEBUG]  Stored integer %d to register %d \n", num, reg );
+#endif
 }
 
 
@@ -73,7 +183,7 @@ int invm_Opcodes_Set( invm_t *p )
    p->opcodes[AND] = NULL;
    p->opcodes[OR] = NULL;
 
-   p->opcodes[INT_STORE] = NULL;
+   p->opcodes[INT_STORE] = vm_int_store;
    p->opcodes[INT_PRINT] = NULL;
    p->opcodes[INT_TOSTRING] = NULL;
    p->opcodes[INT_RANDOM] = NULL;
